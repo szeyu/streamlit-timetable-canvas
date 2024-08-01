4b8f8f79e9892a48c84081fddbfe5bf1f7dfd698
szeyu <szeyusim@gmail.com>
2024-08-01 15:27:01 +0800

    add setup.py and resources folder

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlit-timetable-canvas",
+    version="0.0.1",
+    author="Sze Yu Sim",
+    author_email="szeyu.sim@embeddedllm.com",
+    description="Streamlit component that allows you to generate timetable canvas",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'timetable_selector': [
+            'frontend/build/*',
+        ],
+    },
+    install_requires=[
+        'streamlit>=0.63',
+    ],
+)
